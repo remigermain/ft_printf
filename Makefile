af835c2b6b6798725b5c34f1d8ce0744bd93dc87
Remi Germain <rgermain@z1r1p4.le-101.fr>
2018-12-19 23:16:34 +0100

    ft_printf.c

--- Makefile
+++ Makefile
@@ -6,26 +6,26 @@
 #    By: rgermain <marvin@le-101.fr>                +:+   +:    +:    +:+      #
 #                                                  #+#   #+    #+    #+#       #
 #    Created: 2018/10/01 15:39:03 by rgermain     #+#   ##    ##    #+#        #
-#    Updated: 2018/12/19 23:02:17 by rgermain    ###    #+. /#+    ###.fr      #
+#    Updated: 2018/12/19 23:00:59 by rgermain    ###    #+. /#+    ###.fr      #
 #                                                          /                   #
 #                                                         /                    #
 # **************************************************************************** #
 
 
-NAME = ./a.out
+NAME = libftprintf.a
 
-CFLAGS = -Wall -Werror -Wextra
+//CFLAGS = -Wall -Werror -Wextra
 
 INCLUDE = -Iincludes
 
 CINC = includes/ft_printf.h libft/includes/libft.h
 
-SRC = main.c ft_printf.c ft_conv.c utils.c utils_print.c utils_double.c \
-		ft_params_nb.c ft_params_char.c ft_params_string.c \
-		ft_params_f.c ft_params_k.c ft_params_ti.c ft_params_ts.c \
-		ft_params_a.c \
-		lst_init.c lst_initnb.c lst_base.c \
-		debug.c
+SRC = ft_printf.c ft_conv.c utils.c utils_print.c utils_double.c \
+	ft_params_nb.c ft_params_char.c ft_params_string.c \
+	ft_params_f.c ft_params_k.c ft_params_ti.c ft_params_ts.c \
+	ft_params_a.c \
+	lst_init.c lst_initnb.c lst_base.c \
+	debug.c
 
 OBJ = $(SRC:.c=.o)
 
@@ -38,22 +38,27 @@ COBJ = $(addprefix $(DOBJ),$(OBJ))
 all: $(NAME)
 
 $(NAME): $(COBJ)
-	@make -C libft/ all
 	@echo "Compilation de l'executable" $(NAME)
-	@gcc $? libft/libft.a -o $(NAME)
+	@ar ru $(NAME) $? libft/obj/*.o
+	@ranlib $(NAME)
 
 $(DOBJ)%.o : $(DSRC)%.c $(CINC)
+	@make -C libft/ all
 	@mkdir -p $(DOBJ)
 	@gcc $(CFLAGS) $(INCLUDE) -c $< -o $@
 	@echo "Compilation de la fonction "$<
 
 clean:
+	@make -C libft/ clean
 	@mkdir -p $(DOBJ)
 	@rm -rf $(DOBJ)
 	@echo "Suppresion des objects"
 
 fclean: clean
+	@make -C libft/ fclean
 	@rm -f $(NAME)
 	@echo "Suppresion de la library "$(NAME)
 
-.PHONY: all clean fclean
+re: fclean all
+
+.PHONY: all clean fclean re
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,50 +6,17 @@
 /*   By: rgermain <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/11 15:09:25 by rgermain     #+#   ##    ##    #+#       */
-/*   Updated: 2018/12/19 19:33:46 by rgermain    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/12/19 23:16:03 by rgermain    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_dprintf(int fd, const char *format, ...)
+static int	ftprintf_base(const char *format, t_valst *lst_va)
 {
-	t_valst		*lst_va;
-	int			i;
+	int	i;
 
-	lst_va = lstva_init(fd);
-	va_start(lst_va->lst_va, format);
-	va_copy(lst_va->copy, lst_va->lst_va);
-	i = 0;
-	while (format[i] != '\0')
-	{
-		if (format[i] == '{')
-			i = ft_printcolor((char*)format, i, lst_va->fd);
-		else if (format[i] == '%')
-			i = ft_conv(lst_va, (char*)format, i, 1);
-		else
-		{
-			ft_putchar_fd(format[i++], lst_va->fd);
-			lst_va->count++;
-		}
-		if (lst_va->count == -1)
-			return (-1);
-	}
-	va_end(lst_va->lst_va);
-	va_end(lst_va->copy);
-	free(lst_va);
-	return (lst_va->count);
-}
-
-int		ft_printf(const char *format, ...)
-{
-	t_valst		*lst_va;
-	int			i;
-
-	lst_va = lstva_init(1);
-	va_start(lst_va->lst_va, format);
-	va_copy(lst_va->copy, lst_va->lst_va);
 	i = 0;
 	while (format[i] != '\0')
 	{
@@ -65,6 +32,33 @@ int		ft_printf(const char *format, ...)
 		if (lst_va->count == -1)
 			return (-1);
 	}
+	return (lst_va->count);
+}
+
+int			ft_dprintf(int fd, const char *format, ...)
+{
+	t_valst	*lst_va;
+	int		count;
+
+	lst_va = lstva_init(fd);
+	va_start(lst_va->lst_va, format);
+	va_copy(lst_va->copy, lst_va->lst_va);
+	count = ftprintf_base(format, lst_va);
+	va_end(lst_va->lst_va);
+	va_end(lst_va->copy);
+	free(lst_va);
+	return (lst_va->count);
+}
+
+int			ft_printf(const char *format, ...)
+{
+	t_valst	*lst_va;
+	int		count;
+
+	lst_va = lstva_init(1);
+	va_start(lst_va->lst_va, format);
+	va_copy(lst_va->copy, lst_va->lst_va);
+	count = ftprintf_base(format, lst_va);
 	va_end(lst_va->lst_va);
 	va_end(lst_va->copy);
 	free(lst_va);
